b8460088919ec86007b43abf050d7cf7f04caa3e
Doc Walker <doc.walker@jameshardie.com>
2013-12-23 10:11:20 -0600

    Fix #3 add spec for dummy rails app

---
+++ spec/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '80cc4f987cf7cfda06c21624c25b808fc420ae80144cb9425c8f24f169c38cdce749ab3c2c711ce87178a0191127dd62c6907aa2154642de78624226cd529e39'
