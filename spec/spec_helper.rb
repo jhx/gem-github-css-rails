3cab69e946f9d090d3a2c234975c10ba0ebca0d4
Doc Walker <doc.walker@jameshardie.com>
2013-12-24 13:34:12 -0600

    Fix #7 update to match coding style of jhx/gem-*

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@ require 'capybara/rspec'
 require 'rspec/rails'
 require 'rspec/autorun'
 
+include Sprockets::Helpers::RailsHelper
+include Sprockets::Helpers::IsolatedHelper
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
