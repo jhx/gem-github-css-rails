64b0e341b96bb84fec590d6a7f48d0888f6095bb
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 15:24:20 -0600

    Fix rubocop TrailingWhitespace offenses

--- spec/features/asset_pipeline_spec.rb
+++ spec/features/asset_pipeline_spec.rb
@@ -4,7 +4,7 @@ feature 'Asset pipeline' do
   scenario 'Browser requests application stylesheet' do
     visit asset_path('application.css')
     # save_and_open_page  # for debugging (opens page in browser)
-    
+
     expect(page).to have_text(<<-EOF
     /* Github Markdown Stylesheet */
     /* Based on https://gist.github.com/3331384 */
