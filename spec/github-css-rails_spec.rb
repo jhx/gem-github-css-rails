2d761edd1af73163d4ee3691773db7486b009beb
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 15:13:43 -0600

    Fix rubocop MethodCallParentheses offense

--- spec/github-css-rails_spec.rb
+++ spec/github-css-rails_spec.rb
@@ -5,7 +5,7 @@ describe GithubCss::Rails do
   context 'VERSION constant' do
     it 'is a valid SemVer string (semver.org)' do
       version = GithubCss::Rails::VERSION
-      expect { Semantic::Version.new version }.to_not raise_error()
+      expect { Semantic::Version.new version }.to_not raise_error
     end # it
   end # context
 end # describe
